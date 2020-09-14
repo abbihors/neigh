b5cecc6661f5f7d1d81ae7e6bf2d5c67faf69ce8
abbiwyn <abbiwyn@gmail.com>
2020-09-14 15:41:39 +0300

    Modularized vibrator

--- neigh/main.py
+++ neigh/main.py
@@ -1,27 +1,16 @@
-from collections import deque
-from datetime import datetime
 import asyncio
-import audioop
-import json
-import math
-import os
 import concurrent
-import subprocess
-import sys
 import time
-import wave
+import random
 
-from buttplug.client import ButtplugClient
-from buttplug.client import ButtplugClientWebsocketConnector
 from playsound import playsound
 from tensorflow.keras.models import load_model
 import librosa
 import numpy as np
 
 from recorder import Recorder
+from vibrator import Vibrator
 import settings
-
-# --------------------------------- Constants -------------------------------- #
 
 # Audio format settings
 SAMPLE_RATE = 16000
@@ -33,61 +22,6 @@ MAX_SILENCE_S = 0.1
 
 # Seconds of audio to save before recording (to avoid cutting the start)
 PREV_AUDIO_S = 0.2
-
-# The time period over which to measure frequency, in seconds
-# TODO: Clean this up, this makes no sense
-SPEECH_FREQUENCY_SAMPLING_INTERVAL = 60
-
-# --------------------------------- Vibration -------------------------------- #
-
-def calculate_vibration_strength(curve, volume, recent_speech_count):
-    return curve(volume, recent_speech_count)
-    
-def curve_linear(volume, recent_speech_count):
-    return min(1.0, round(volume / settings.max_expected_vol, 2))
-
-def curve_evil(volume, recent_speech_count):
-    buildup_count = settings.buildup_count
-    max_expected_vol = settings.max_expected_vol
-
-    # Sigmoid function to make it extra evil
-    vibration_strength = 1 / (1 + (math.e ** ((-volume / (max_expected_vol / 10)) + 5)))
-
-    speech_frequency = recent_speech_count / SPEECH_FREQUENCY_SAMPLING_INTERVAL
-
-    # 20 caws per minute to get max effect = freq = 0.333
-    frequency_multiplier = min(1.0,
-        speech_frequency /
-            (buildup_count / SPEECH_FREQUENCY_SAMPLING_INTERVAL))
-
-    vibration_strength = vibration_strength * (0.5 + (0.5 * frequency_multiplier))
-    vibration_strength = round(vibration_strength, 2) # 2 decimal places = 100 values between 0 and 1
-
-    return vibration_strength
-
-# ------------------------------ Buttplug stuff ------------------------------ #
-
-async def start_buttplug_server():
-    await asyncio.create_subprocess_exec(settings.server_path, "--wsinsecureport", "12345")
-    await asyncio.sleep(1) # Wait for the server to start up
-    print('Buttplug server started')
-
-async def init_buttplug_client():
-    client = ButtplugClient("Neigh")
-    connector = ButtplugClientWebsocketConnector("ws://127.0.0.1:12345")
-
-    await client.connect(connector)
-    await client.start_scanning()
-
-    # Wait until we get a device
-    while client.devices == {}:
-        await asyncio.sleep(1)
-
-    await client.stop_scanning()
-
-    return client
-
-# ----------------------------------- Misc ----------------------------------- #
 
 def predict_class(model, sample_bytes):
     # Keras model expects an array of floats
@@ -101,34 +35,19 @@ def predict_class(model, sample_bytes):
     labels = ['animal', 'other']
     return sorted(labels)[prediction]
 
-# This runs in the background and waits for things to be put in the queue
-async def vibrate_worker(queue, bp_device):
-    while True:
-        vibration_strength = await queue.get()
-
-        vibration_strength = max(0.1, vibration_strength)
-        await bp_device.send_vibrate_cmd(vibration_strength)
-        queue.task_done()
-        await asyncio.sleep(1)
-        await bp_device.send_stop_device_cmd()
-
-# ------------------------------- Main function ------------------------------ #
+async def vibrate_random(vibrator):
+    amount = round(random.uniform(0.3, 1.0), 2)
+    duration = round(random.uniform(1.0, 1.5), 2)
+    print(f'Vibrating: {amount}, {duration}s')
+    await vibrator.vibrate(amount, duration)
 
 async def main():
-    await start_buttplug_server()
-
-    bp_client = await init_buttplug_client()
-    bp_device = bp_client.devices[0] # Just get the first device
-
-    queue = asyncio.Queue()
-    asyncio.create_task(vibrate_worker(queue, bp_device))
-
     model = load_model(settings.model_path)
-    speech_timestamps = []
-
+    vibrator = await Vibrator.create()
     recorder = Recorder(samplerate=SAMPLE_RATE, channels=CHANNELS, dtype='int16')
 
     print('Neigh: Listening...')
+    await vibrator.vibrate(0.2, 0.2) # Do a little vibration to confirm its working
 
     while True:
         # Run the recorder in a separate thread to prevent blocking everything while it runs
@@ -138,22 +57,12 @@ async def main():
 
         recorder.trim_or_pad(1.0)
         predicted_class = predict_class(model, recorder.get_bytes())
-
+        
         if (predicted_class == 'animal'):
-            volume = recorder.get_rms_volume()
-            
-            # Add timestamp
-            speech_timestamps.append(datetime.now())
-            
-            # Remove old timestamps
-            speech_timestamps = [ts for ts in speech_timestamps
-                if (datetime.now() - ts).seconds < SPEECH_FREQUENCY_SAMPLING_INTERVAL]
+            await vibrate_random(vibrator)
 
-            # Do fun stuff!
-            vibration_strength = calculate_vibration_strength(curve_evil, volume, len(speech_timestamps))
-            await queue.put(vibration_strength)
-            
-            print(f'Got animal sound, vol: {volume}, vibe: {vibration_strength}')
+            volume = recorder.get_rms_volume()
+            print(f'Got animal sound, vol: {volume}')
             # playsound('~/dev/soundfx/quake_hitsound.mp3')
             
         # Save recordings to help improve model
@@ -161,5 +70,4 @@ async def main():
         filename = f'{settings.recordings_path}/{predicted_class}/output_{epoch_time}.wav'
         recorder.write_wav(filename)
 
-# Start program
 asyncio.run(main())
---
+++ neigh/vibrator.py
@@ -0,0 +1,64 @@
+import asyncio
+
+from buttplug.client import ButtplugClient
+from buttplug.client import ButtplugClientWebsocketConnector
+
+class Vibrator():
+
+    # We need to use a factory method here because async constructors don't work
+    @classmethod
+    async def create(cls):
+        self = Vibrator()
+        
+        await start_buttplug_server()
+
+        self._bp_client = await init_buttplug_client()
+        self._bp_device = self._bp_client.devices[0] # Just get the first device
+
+        self._vibrate_queue = asyncio.Queue()
+        self._vibration_level = 0.0
+
+        asyncio.create_task(self.update_vibrator())
+        
+        return self
+
+    # This runs in the background and waits for things to be put in the queue
+    async def update_vibrator(self):
+        while True:
+            amount, duration = await self._vibrate_queue.get()
+
+            await self._bp_device.send_vibrate_cmd(amount)
+            self._vibrate_queue.task_done()
+            await asyncio.sleep(duration)
+
+            if self._vibrate_queue.empty():
+                await self._bp_device.send_vibrate_cmd(self._vibration_level)
+
+    async def vibrate(self, amount, duration):
+        await self._vibrate_queue.put([amount, duration])
+
+    async def set_vibration_level(self, amount):
+        self._vibration_level = amount
+        await self._bp_device.send_vibrate_cmd(self._vibration_level)
+
+    async def stop(self):
+        await self._bp_device.send_stop_device_cmd()
+
+
+async def start_buttplug_server():
+    await asyncio.create_subprocess_exec('intiface-cli', "--wsinsecureport", "12345")
+    await asyncio.sleep(1) # Wait for the server to start up
+
+async def init_buttplug_client():
+    client = ButtplugClient("Neigh")
+    connector = ButtplugClientWebsocketConnector("ws://127.0.0.1:12345")
+
+    await client.connect(connector)
+    await client.start_scanning()
+
+    # Wait until we get a device
+    while client.devices == {}:
+        await asyncio.sleep(1)
+
+    await client.stop_scanning()
+    return client
